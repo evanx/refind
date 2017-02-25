0162ae210b9416a42a0be9e5882b0b28102c1146
Evan Summers <evan.summers@gmail.com>
2017-02-25 14:36:45 +0000

    initial

--- bin/test.sh
+++ bin/test.sh
@@ -3,49 +3,40 @@ set -u -e
 
 PATH=$PATH:./node_modules/.bin
 
-name='scan-llen'
+name='resend'
 network="$name-network"
 redisName="$name-redis"
 
 tearDown() {
-  docker-rm $redisName
-  docker-network-rm $network
+  docker-rm-redis $name
 }
 
 setUp() {
-  docker network create -d bridge scan-llen-network
-  redisContainer=`docker run --network=scan-llen-network \
-      --name $redisName -d redis`
-  redisHost=`docker inspect $redisContainer |
-      grep '"IPAddress":' | tail -1 | sed 's/.*"\([0-9\.]*\)",/\1/'`
-}
-
-populate() {
-  redis-cli -h $redisHost lpush list1 1 | grep -q '^1$'
-  redis-cli -h $redisHost lpush list2 1 | grep -q '^1$'
-  redis-cli -h $redisHost lpush list2 2 | grep -q '^2$'
+  redisHost=`docker-run-redis $name`
 }
 
 build() {
-  docker build -t scan-llen https://github.com/evanx/scan-llen.git
+  docker build -t resend https://github.com/evanx/resend.git
 }
 
-run() {
-  docker run --name scan-llen-instance --rm -i \
-    --network=scan-llen-network \
-    -e host=$redisHost \
-    -e pattern='list*' \
-    scan-llen
+start() {
+  docker run --name resend-instance -d \
+    --network=resend-network \
+    -e redisHost=$redisHost \
+    resend
+}
+
+stop() {
+  docker rm -f resend-instance
 }
 
 main() {
     tearDown
     setUp
     sleep 1
-    populate
     build
-    run | grep '^1 list1$'
-    run | grep '^2 list2$'
+    start
+    stop
     tearDown
     echo 'OK'
 }
