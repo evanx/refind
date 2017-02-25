9f2dccc08a7296e3669a2a4f20a6812346d42821
Evan Summers <evan.summers@gmail.com>
2017-02-25 19:26:52 +0000

    initial

--- bin/test.sh
+++ bin/test.sh
@@ -8,37 +8,43 @@ network="$name-network"
 redisName="$name-redis"
 
 tearDown() {
-  docker-rm-redis $name
+   docker-rm-redis $name
 }
 
 setUp() {
-  redisHost=`docker-run-redis $name`
+   redisHost=`docker-run-redis $name`
 }
 
 build() {
-  docker build -t refind https://github.com/evanx/refind.git
+   docker build -t refind https://github.com/evanx/refind.git
 }
 
 start() {
-  docker run --name refind-instance -d \
-    --network=refind-network \
-    -e redisHost=$redisHost \
-    refind
+   docker run --name refind-instance -d \
+      --network=refind-network \
+      -e redisHost=$redisHost \
+      -v /tmp/test/volumes/refile/data:/data \
+      refind
+}
+
+test() {
+   curl -s http://localhost:8871/re/analytics | grep '"counts":'
 }
 
 stop() {
-  docker rm -f refind-instance
+   docker rm -f refind-instance
 }
 
 main() {
-    tearDown
-    setUp
-    sleep 1
-    build
-    start
-    stop
-    tearDown
-    echo 'OK'
+   tearDown
+   setUp
+   sleep 1
+   build
+   start
+   test
+   stop
+   tearDown
+   echo 'OK'
 }
 
 main
